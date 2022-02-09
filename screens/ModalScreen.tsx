2e59fede7fd1186b62a13f62e9219543cbaea862
Ciaran Curran <ciaran@Ciarans-MacBook-Air.local>
2022-02-09 19:24:33 +0000

    feat: add welcome message

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
   title: {
     fontSize: 20,
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,21 +1,35 @@
 import { StyleSheet } from "react-native";
-
+import { FontAwesome5 } from "@expo/vector-icons";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import Colors from "../constants/Colors";
 
 export default function TabOneScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Welcome to my app</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View
+        style={{
+          position: "absolute",
+          bottom: 50,
+          right: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "orange", margin: 5 }}>
+          Find instructors here!
+        </Text>
+        <FontAwesome5 name="hand-point-down" size={30} color={"orange"} />
+      </View>
     </View>
   );
 }
